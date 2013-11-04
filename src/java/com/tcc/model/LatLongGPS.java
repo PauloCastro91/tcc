5b453a1ddd746c37d3deafbb0bdd528e84177564
paulo.castro <paulo.castro@PauloPC.futurekids.edu>
2013-11-04 08:00:28 -0200

    Parte dos Graficos

---
+++ src/java/com/tcc/model/LatLongGPS.java
@@ -0,0 +1,89 @@
+package com.tcc.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class LatLongGPS {
+   
+    private Integer id;
+    private String latitude;
+    private String longitude;
+    private List<HistoricoCoodenadas> historicoCoodenadas;
+    
+    public LatLongGPS(){
+        this.historicoCoodenadas = new ArrayList<HistoricoCoodenadas>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<HistoricoCoodenadas> getHistoricoCoodenadas() {
+        return historicoCoodenadas;
+    }
+
+    public void setHistoricoCoodenadas(List<HistoricoCoodenadas> historicoCoodenadas) {
+        this.historicoCoodenadas = historicoCoodenadas;
+    }
+    
+    public void addCoordenadas(String string, String string0) {
+        HistoricoCoodenadas historicoCoodenada = new HistoricoCoodenadas();
+        historicoCoodenada.order = (this.historicoCoodenadas.size() + 1);
+        historicoCoodenada.latitude = string;
+        historicoCoodenada.longitude = string0;
+        this.historicoCoodenadas.add(historicoCoodenada);
+    }
+    
+    public class HistoricoCoodenadas {
+        
+        private Integer order;
+        private String latitude;
+        private String longitude;
+
+        public Integer getOrder() {
+            return order;
+        }
+
+        public void setOrder(Integer order) {
+            this.order = order;
+        }
+
+        public String getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(String latitude) {
+            this.latitude = latitude;
+        }
+
+        public String getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(String longitude) {
+            this.longitude = longitude;
+        }
+    }
+}
---
+++ src/java/com/tcc/webService/Gps.java
@@ -0,0 +1,83 @@
+package com.tcc.webService;
+
+import com.tcc.model.LatLongGPS;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.faces.bean.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@RequestScoped
+@Path("/gps")
+public class Gps {
+
+    static final private Map<Integer, LatLongGPS> eventosMap;
+
+    static {
+        eventosMap = new HashMap<Integer, LatLongGPS>();
+
+        LatLongGPS latLongGPS = new LatLongGPS();
+        latLongGPS.setId(1); // Código do aparelho
+        latLongGPS.setLatitude("-30.030442381294925"); //Latitude atual
+        latLongGPS.setLongitude("-51.20699644088745"); //Longetude atual
+
+        latLongGPS.addCoordenadas("-20.030442381294925", "-45.20699644088745");
+        latLongGPS.addCoordenadas("-30.030442381294925", "-51.20699644088745");
+
+        eventosMap.put(latLongGPS.getId(), latLongGPS);
+    }
+
+    @GET
+    @Produces("text/json")
+    public List<LatLongGPS> getEventos() {
+        return new ArrayList<LatLongGPS>(eventosMap.values());
+    }
+
+    @POST
+    @Path("/post")
+    @Consumes("text/json")
+    @Produces("text/plain")
+    public String adiciona(LatLongGPS latLongGPS) {
+        LatLongGPS get = eventosMap.get(latLongGPS.getId());
+        if (get != null) {
+            get.setLatitude(latLongGPS.getLatitude());
+            get.setLongitude(latLongGPS.getLongitude());
+            get.addCoordenadas(latLongGPS.getLatitude(), latLongGPS.getLongitude());
+        } else {
+            latLongGPS.setId(latLongGPS.getId());
+            eventosMap.put(latLongGPS.getId(), latLongGPS);
+        }
+        return "Adicionado com sucesso.";
+    }
+
+    @GET
+    @Path("/get")
+    @Produces("text/json")
+    public List<LatLongGPS> getCoodenadas() {
+        return new ArrayList<LatLongGPS>(eventosMap.values());
+    }
+//
+//    @Path("{id}")
+//    @PUT
+//    @Consumes("text/xml")
+//    @Produces("text/plain")
+//    public String atualizaEvento(Evento evento, @PathParam("id") int id) {
+//        Evento atual = eventosMap.get(id);
+//        atual.setNome(evento.getNome());
+//        atual.setInicio(evento.getInicio());
+//        return evento.getNome() + " atualizada.\n";
+//    }
+//
+//    @Path("{id}")
+//    @DELETE
+//    @Produces("text/plain")
+//    public String removeEvento(@PathParam("id") int id) {
+//        eventosMap.remove(id);
+//        return "Evento removido.\n";
+//    }
+}
